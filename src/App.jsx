62c1b862ae91b7ce2f6320da2e50cf39e4a6c59f
mostlyali07 <104094853+mostlyali07@users.noreply.github.com>
2025-02-03 19:07:20 +0500

    update header

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./styles/App.css";
 import "./styles/media.css";
 import Navigation from "./Navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,55 @@
 import React from "react";
-
+import Avatar from "../assets/images/Avatar.png";
+import { FaCalendarAlt, FaEnvelope, FaBell } from "react-icons/fa";
 const Header = () => {
   return (
     <>
       <header className="main-header">
-        Header
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-6 p-0">
+              <h2 className="m-0">
+                Good Morning
+                {/* {{Ali Rehan}} */}!
+              </h2>
+            </div>
+            <div className="col-md-6">
+              <div className="top-bar">
+                <FaCalendarAlt />
+                <FaEnvelope />
+                <FaBell />
+
+                <div className="dropdown">
+                  <button
+                    className="dropdown-toggle"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    <img src={Avatar} alt="Avatar" />
+                  </button>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Settings
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Edit Profile
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Logout
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
     </>
   );
